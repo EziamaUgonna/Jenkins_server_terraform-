beee61dff1c4c9cabb0ed3bc869f1225343fb304
Elvin Ugonna <elvindavin@gmail.com>
2023-06-23 22:12:39 -0400

    Update install_jenkins.sh

--- terraform/modules/compute/install_jenkins.sh
+++ terraform/modules/compute/install_jenkins.sh
@@ -61,3 +61,50 @@ sudo apt-get install curl -y
 sudo  curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.7.0/bin/linux/amd64/kubectl
 sudo  chmod +x ./kubectl
 sudo mkdir -p $HOME/bin && sudo cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin 
+
+
+
+
+/*
+#!/bin/bash
+
+# Install Java
+sudo apt update
+sudo apt install openjdk-11-jre -y
+
+# Install Docker
+sudo apt-get update
+sudo apt-get install docker.io -y
+sudo chmod 666 /var/run/docker.sock
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# Check if Docker is installed
+docker_version=$(docker -v)
+if [[ $docker_version == *"command not found"* ]]; then
+  echo "Docker installation failed"
+  exit 1
+else
+  echo "Docker installed successfully"
+fi
+
+# Pull Jenkins Docker image
+sudo docker pull jenkins/jenkins
+
+# Run Jenkins Docker container
+sudo docker run -d -p 8080:8080 -v jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --name jenkins jenkins/jenkins
+
+# Install kubectl inside Jenkins container
+sudo docker exec -it jenkins bash -c "apt-get update && apt-get install curl -y"
+sudo docker exec -it jenkins bash -c "curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.7.0/bin/linux/amd64/kubectl"
+sudo docker exec -it jenkins bash -c "chmod +x ./kubectl"
+sudo docker exec -it jenkins bash -c "mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin"
+
+# Install AWS IAM Authenticator inside Jenkins container
+sudo docker exec -it jenkins bash -c "curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/linux/amd64/aws-iam-authenticator"
+sudo docker exec -it jenkins bash -c "chmod +x ./aws-iam-authenticator"
+sudo docker exec -it jenkins bash -c "mkdir -p $HOME/bin && cp ./aws-iam-authenticator $HOME/bin/aws-iam-authenticator && export PATH=$PATH:$HOME/bin"
+
+# Restart Jenkins container for changes to take effect
+sudo docker restart jenkins
+*/
